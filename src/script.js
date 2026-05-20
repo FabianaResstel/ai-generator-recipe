79bb8bfb6918eab442b7be92395f8d1dba53acf3
FabianaResstel <fabianaresstel01@gmail.com>
2026-05-20 19:08:42 -0400

    improved the UX

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 function recipeSuggested(response) {
   new Typewriter("#recipe", {
-    strings: [response.data.answer],
+    strings: response.data.answer,
     autoStart: true,
     delay: 20,
+    cursor: "",
   });
 }
 
@@ -11,9 +12,12 @@ function getAi() {
   let prompt1 = document.querySelector("#input-prompt-search");
   let prompt = prompt1.value;
   let context =
-    "Generate a recipe using the ingredient provided by the user. Use simple, common ingredients that most people have at home and make sure to give the instructions of cooking.Format the output in using the follow exemplo, <p>recipe name </br>ingredients</br>, braking each phrase into a new line.";
+    "Generate a recipe using the ingredient provided by the user. Use simple, common ingredients that most people have at home and make sure to give the instructions of cooking.Format the output in using the follow exemplo, <p>recipe name </br></br>ingredients</br>, braking each phrase into a new line.";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let waiting = document.querySelector("#recipe");
+  waiting.innerHTML = "Generating your recipe.....please wait.";
 
   axios.get(apiUrl).then(recipeSuggested);
 }
